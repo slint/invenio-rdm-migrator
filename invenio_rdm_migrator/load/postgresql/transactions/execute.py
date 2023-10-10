943ccf42a8af2c7cad877f6bf11167c3185b7f2f
Alex Ioannidis <a.ioannidis@cern.ch>
2023-10-10 22:49:55 +0200

    actions: split processing for better logging

--- invenio_rdm_migrator/load/postgresql/transactions/execute.py
+++ invenio_rdm_migrator/load/postgresql/transactions/execute.py
@@ -104,73 +104,16 @@ class PostgreSQLTx(Load):
 
     def _load(self, transactions):
         """Performs the operations of a group transaction."""
-        exec_kwargs = dict(execution_options={"synchronize_session": False})
+        self._stats.start = datetime.utcnow()
 
         outer_trans = None
         if self.dry:
-            self.logger.warn("Running Tx loading in dry mode inside transaction!")
+            self.logger.warn("Running Tx loading in dry mode inside a transaction!")
             outer_trans = self.session.begin()
+
         try:
-            self._stats.start = datetime.utcnow()
             for action in transactions:
-                tx = action.tx
-                self.logger.info(
-                    f"BEGIN | [{action.transform_name} -> {action.name}] from "
-                    f"Tx {tx and tx.id} (LSN: {tx and tx.commit_lsn})"
-                )
-                with self.session.no_autoflush:
-                    nested_trans = self.session.begin_nested()
-                    try:
-                        for op in action.prepare(session=self.session):
-                            if op.type == OperationType.INSERT:
-                                row = op.as_row_dict()
-                                self.logger.debug(f"INSERT {op.model}: {row}")
-                                self.session.execute(
-                                    sa.insert(op.model),
-                                    [row],
-                                    **exec_kwargs,
-                                )
-                            elif op.type == OperationType.DELETE:
-                                self.logger.debug(f"DELETE {op.model}: {op.data}")
-                                self.session.execute(
-                                    sa.delete(op.model).where(*op.pk_clauses),
-                                    **exec_kwargs,
-                                )
-                            elif op.type == OperationType.UPDATE:
-                                row = op.as_row_dict()
-                                self.logger.debug(f"UPDATE {op.model}: {op.data}")
-                                self.session.execute(
-                                    sa.update(op.model),
-                                    [row],
-                                    **exec_kwargs,
-                                )
-                            self.session.flush()
-                            self._stats.ops += 1
-                        nested_trans.commit()
-                        self._stats.tx += 1
-                    except Exception:
-                        self.logger.exception(
-                            f"Could not load [{action.transform_name} -> {action.name}]"
-                            f"{action.data}",
-                            exc_info=True,
-                        )
-                        self.tx_logger.exception(
-                            "Failed processing transaction",
-                            extra={
-                                "tx": action.tx,
-                                "data": action.data,
-                                "transform": action.transform_name,
-                                "load": action.name,
-                            },
-                            exc_info=True,
-                        )
-                        nested_trans.rollback()
-                        if self.raise_on_db_error:
-                            raise
-                self.logger.info(
-                    f"END   | [{action.transform_name} -> {action.name}] from "
-                    f"Tx {tx and tx.id} (LSN: {tx and tx.commit_lsn})"
-                )
+                self._process_action(action)
                 self.logger.info(self._stats)
         except Exception:
             self.logger.exception("Transactions load failed", exc_info=True)
@@ -181,6 +124,82 @@ class PostgreSQLTx(Load):
         finally:
             if self.dry and outer_trans:
                 outer_trans.rollback()
+
+    def _process_action(self, action):
+        """Process an action."""
+        tx = action.tx
+        self.logger.info(
+            f"BEGIN | [{action.transform_name} -> {action.name}] from "
+            f"Tx {tx and tx.id} (LSN: {tx and tx.commit_lsn})"
+        )
+
+        with self.session.no_autoflush:
+            nested_trans = self.session.begin_nested()
+            try:
+                for op in action.prepare(session=self.session):
+                    try:
+                        self._execute_op(op)
+                    except Exception:
+                        self.logger.exception(
+                            "Failed to execute operation "
+                            f"[{action.transform_name} -> {action.name}]: {op}",
+                            exc_info=True,
+                        )
+                        raise
+                nested_trans.commit()
+                self._stats.tx += 1
+            except Exception:
+                self.logger.exception(
+                    f"Failed to load [{action.transform_name} -> {action.name}] "
+                    f"{action.data}",
+                    exc_info=True,
+                )
+                self.tx_logger.exception(
+                    "Failed processing transaction",
+                    extra={
+                        "tx": action.tx,
+                        "data": action.data,
+                        "transform": action.transform_name,
+                        "load": action.name,
+                    },
+                    exc_info=True,
+                )
+                nested_trans.rollback()
+                if self.raise_on_db_error:
+                    raise
+
+        self.logger.info(
+            f"END   | [{action.transform_name} -> {action.name}] from "
+            f"Tx {tx and tx.id} (LSN: {tx and tx.commit_lsn})"
+        )
+
+    def _execute_op(self, op):
+        """Execute an SQL operation and flush."""
+        exec_kwargs = dict(execution_options={"synchronize_session": False})
+        if op.type == OperationType.INSERT:
+            row = op.as_row_dict()
+            self.logger.debug(f"INSERT {op.model}: {row}")
+            self.session.execute(
+                sa.insert(op.model),
+                [row],
+                **exec_kwargs,
+            )
+        elif op.type == OperationType.DELETE:
+            self.logger.debug(f"DELETE {op.model}: {op.data}")
+            self.session.execute(
+                sa.delete(op.model).where(*op.pk_clauses),
+                **exec_kwargs,
+            )
+        elif op.type == OperationType.UPDATE:
+            row = op.as_row_dict()
+            self.logger.debug(f"UPDATE {op.model}: {op.data}")
+            self.session.execute(
+                sa.update(op.model),
+                [row],
+                **exec_kwargs,
+            )
+        self.session.flush()
+        self._stats.ops += 1
 
     def run(self, entries, cleanup=False):
         """Load entries."""
